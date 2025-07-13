363c7e1ecb6f4315f74feea4a3934a80a9e8741f
Praxcedes <praxcedeskabeya@gmail.com>
2025-07-13 12:29:24 -0700

    phase-1-sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Append the ID to the DOM
+    const body = document.querySelector("body");
+    const p = document.createElement("p");
+    p.textContent = data.id;
+    body.appendChild(p);
+  })
+  .catch(error => {
+    // Append error message to the DOM
+    const body = document.querySelector("body");
+    const p = document.createElement("p");
+    p.textContent = error.message;
+    body.appendChild(p);
+  });
+}
+
